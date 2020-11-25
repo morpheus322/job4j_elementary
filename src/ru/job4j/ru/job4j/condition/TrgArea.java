ddd5d20c151c6ebd202a8d1d441748e413c0b63b
Morpheus322 <kopirka2015@mail.ru>
2020-11-25 23:01:39 +0500

    3.2. Фиксация изменений на github [#353805]

---
+++ src/ru/job4j/ru/job4j/condition/TrgArea.java
@@ -0,0 +1,18 @@
+package ru.job4j.ru.job4j.condition;
+
+public class TrgArea {
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        double first = p - a;
+        double second = p - b;
+        double three = p - c;
+        double four = p * first * second * three;
+        double rsl = Math.sqrt(four);
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double rsl = TrgArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + rsl);
+    }
+}
