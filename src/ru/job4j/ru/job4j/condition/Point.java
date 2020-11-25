c6a8fcc96fdf74c4bbbad2e008850ffff7b6ae66
Morpheus322 <kopirka2015@mail.ru>
2020-11-25 23:01:39 +0500

    3.2. Фиксация изменений на github [#353805]

--- src/ru/job4j/ru/job4j/condition/Point.java
+++ src/ru/job4j/ru/job4j/condition/Point.java
@@ -2,13 +2,17 @@ package ru.job4j.ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl =Math.sqrt(Math.pow((x2-x1), 2))+(Math.pow((y2-y1),2));
-        return rsl;
+        double first = x2 - x1;
+        double second = y2 - y1;
+        double three = Math.pow(first, 2);
+        double four = Math.pow(second, 2);
+        double five = three + four;
+        double rls =Math.sqrt(five);
+        return rls;
     }
-
     public static void main(String[] args) {
-        double result = Point.distance(2, 2, 4, 2);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+        double result = Point.distance(1, 2, 8, 6);
+        System.out.println("result (1, 2) to (8, 6) " + result);
     }
 }
 
