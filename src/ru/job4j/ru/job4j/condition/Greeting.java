5054988c25978367ec968d7dde5427eb80e1ea5d
Morpheus322 <kopirka2015@mail.ru>
2020-11-27 14:55:46 +0500

    3.2. Фиксация изменений на github [#353805]

--- src/ru/job4j/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/ru/job4j/condition/Greeting.java
@@ -2,11 +2,9 @@ package ru.job4j.ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!";
-        System.out.println(idea);
-        idea = "But I am a newbie.";
+        String a = "But I am a newbie.";
         int year = 2020;
-        String id = idea + year;
-        System.out.println(id);
+        String idea = "I like Java!" + a + year;
+        System.out.println(idea);
     }
 }
