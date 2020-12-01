588b6f80df474ff999820b02e4c536ac1ea933a7
Morpheus322 <kopirka2015@mail.ru>
2020-12-01 11:38:43 +0500

    3.2. Фиксация изменений на github [#353805]

--- src/ru/job4j/ru/job4j/ru/job4j/converter/Converter.java
+++ src/ru/job4j/ru/job4j/ru/job4j/converter/Converter.java
@@ -7,15 +7,20 @@ public class Converter {
     }
 
     public static int rubleToDollar(int value) {
-        int rsl = value / 60;
+        int rsl = value / 50;
         return rsl;
     }
 
     public static void main(String[] args) {
-        int in = 140;
-        int expected = 2;
-        int out = Converter.rubleToEuro(in);
-        boolean passed = expected == out;
-        System.out.println("140 rubles are 2. Test result : " + passed);
+        int in1 = 140;
+        int expected1 = 2;
+        int out1 = Converter.rubleToEuro(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("140 rubles are 2. Test result : " + passed1);
+        int in2 = 150;
+        int expected2 = 3;
+        int out2 = Converter.rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("150 rubles are 3. Test result : " + passed2);
     }
 }
