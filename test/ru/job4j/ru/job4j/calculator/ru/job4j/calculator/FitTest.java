81ae3e4bc9a5dfe061d04ee05a246dd625f4d040
Morpheus322 <kopirka2015@mail.ru>
2020-12-24 21:46:53 +0500

    0. Операции сравнения [#353729]

--- test/ru/job4j/ru/job4j/calculator/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/ru/job4j/calculator/ru/job4j/calculator/FitTest.java
@@ -7,14 +7,6 @@ import org.junit.Assert;
 public class FitTest {
 
     @Test
-    public void whenMan180Then92() {
-        short in = 180;
-        double expected = 92;
-        double out = Fit.manWeight(in);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
     public void whenMan170Then80() {
         short in = 170;
         double expected = 80.5;
