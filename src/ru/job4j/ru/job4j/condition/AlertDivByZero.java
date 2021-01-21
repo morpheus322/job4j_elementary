f31621ea763fd070a911e307ee4c8d991c6f0f61
Morpheus322 <kopirka2015@mail.ru>
2021-01-22 00:30:03 +0500

    4.1. Операторы ветвлений [#353742]

--- src/ru/job4j/ru/job4j/condition/AlertDivByZero.java
+++ src/ru/job4j/ru/job4j/condition/AlertDivByZero.java
@@ -2,12 +2,12 @@ package ru.job4j.ru.job4j.condition;
 
 public class AlertDivByZero {
     public static void main(String[] args) {
+        possibleDiv(-2);
         possibleDiv(4);
-        possibleDiv(0);
     }
 
     public static void possibleDiv(int number) {
-        if (number == 0) {
+        if (number < 0) {
             System.out.println("This is negative numbers");
         }
     }
